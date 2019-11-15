dedb82c4ff0b6a76a012705233e80fb82bc02601
draganasavanovic <dragana.savanovic@wayseven.com>
2019-11-15 10:19:27 +0100

    created first component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import Greet from "./components/Greet";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>GaGa</p>
-      </header>
+      <Greet></Greet>
     </div>
   );
 }
