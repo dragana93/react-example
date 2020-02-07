7f93e06a98144da479aca87092811e8a51905880
draganasavanovic <dragana.savanovic@wayseven.com>
2020-02-07 10:32:31 +0100

    Portals

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ import ParentComp from "./components/ParentComp";
 import RefsDemo from "./components/RefsDemo";
 import FocusInput from "./components/FocusInput";
 import FRParentInput from "./components/FRParentInput";
+import PortalDemo from "./components/PortalDemo";
 
 function App() {
   return (
@@ -36,7 +37,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
 
-      <FRParentInput />
+      <PortalDemo />
+
+      {/* <FRParentInput /> */}
 
       {/* <FocusInput /> */}
 
